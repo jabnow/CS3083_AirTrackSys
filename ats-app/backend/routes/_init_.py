609879f8c556369ce97a533c416458490a4c25fe
= <=>
2025-04-26 17:12:44 -0400

    fixed models.py

--- ats-app/backend/routes/_init_.py
+++ ats-app/backend/routes/_init_.py
@@ -1,1 +1,6 @@
 # makes routes folder a package, so it can be imported and executed at once hopefully
+
+__all__ = [
+    'auth_bp', 'airplanes_bp', 'airports_bp', 'customers_bp',
+    'flights_bp', 'public_flights_bp', 'purchases_bp', 'tickets_bp'
+]
