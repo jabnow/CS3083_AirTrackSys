9d68c9c9d7420b59838a4c55c2d05212cacccf5d
zeedorien <dorien.zhang@gmail.com>
2025-04-29 14:29:43 -0400

    didn't do anything for now

---
+++ ats-app/frontend/src/CancelTrip.jsx
@@ -0,0 +1,7 @@
+/* 
+Need tickets table, purchased by a customer (so based on their customer email),
+ticket should become purchaseable again in the tickets table
+
+(Can't cancel if the flight departs in <24 hours, but this isn't a frontend 
+thing)
+*/
