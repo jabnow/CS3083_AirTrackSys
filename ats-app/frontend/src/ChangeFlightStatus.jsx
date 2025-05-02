9867a636a9dd3e9b166cb611d6bbeecbdc40b949
= <=>
2025-05-02 10:18:48 -0400

    nvm the connection is not there on react with the DB

--- ats-app/frontend/src/ChangeFlightStatus.jsx
+++ ats-app/frontend/src/ChangeFlightStatus.jsx
@@ -30,7 +30,7 @@ export default function ChangeFlightStatus() {
 
     try {
       const res = await axios.post(
-        'http://localhost:5000/api/flights/status',
+        'http://localhost:5000/api/flights_api/status',
         payload,
         { withCredentials: true }
       );
